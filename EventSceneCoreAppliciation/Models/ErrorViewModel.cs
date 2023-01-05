ccb12f08f919983e97670b26aea959ef9c1f55f4
elifunver <elifnurunver@gmail.com>
2023-01-05 14:49:34 +0300

    Add project files.

---
+++ EventSceneCoreAppliciation/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TiyatroProje.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
