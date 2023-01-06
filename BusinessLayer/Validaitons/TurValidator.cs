822580c68b0a981fa906004daa08aeceb9a5b0d8
elifunver <elifnurunver@gmail.com>
2023-01-06 14:52:59 +0300

    Validations oluşturuldu.

--- BusinessLayer/Validaitons/TurValidator.cs
+++ BusinessLayer/Validaitons/TurValidator.cs
@@ -1,13 +1,14 @@
 ﻿using EntityLayer;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccessLayer.Abstract
+namespace BusinessLayer.Validaitons
 {
-    public interface ISeyirciDal : IGenericDal<Seyirci>
+    public class TurValidator : AbstractValidator<Tur>
     {
     }
 }
