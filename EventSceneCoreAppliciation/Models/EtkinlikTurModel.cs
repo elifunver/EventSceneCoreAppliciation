6b386b0d91da3cfc7fc6c461affc79f7c110a7af
elifunver <elifnurunver@gmail.com>
2023-01-11 15:23:27 +0300

    EtkinlikTurModel yazıldı.

---
+++ EventSceneCoreAppliciation/Models/EtkinlikTurModel.cs
@@ -0,0 +1,10 @@
+﻿using EntityLayer;
+
+namespace EventSceneCoreAppliciation.Models
+{
+    public class EtkinlikTurModel
+    {
+        public Etkinlik etkinlikModal { get; set; }
+        public IEnumerable<Tur> turModal { get; set; }
+    }
+}
