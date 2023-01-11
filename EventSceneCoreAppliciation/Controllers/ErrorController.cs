2dbf554d354a0ecc45786f1c735a43e3175eaaa0
elifunver <elifnurunver@gmail.com>
2023-01-11 14:18:37 +0300

    Error Controller yazıldı.

--- EventSceneCoreAppliciation/Controllers/ErrorController.cs
+++ EventSceneCoreAppliciation/Controllers/ErrorController.cs
@@ -1,6 +1,28 @@
-﻿namespace EventSceneCoreAppliciation.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EventSceneCoreAppliciation.Controllers
 {
-    public class ErrorController
+    [Route("error")]
+    public class ErrorController : Controller
     {
+        [Route("/Error/HandleError/{code:int}")]
+        public IActionResult HandleError(int code)
+        {
+
+
+            ViewBag.code = code.ToString();
+            switch (code)
+            {
+                case 404:
+                    ViewBag.msg = "Uh Oh! Page not found!";
+                    break;
+                case 500:
+                    ViewBag.msg = "Uh Oh! Internal Error!";
+                    break;
+            }
+
+            return View();
+        }
+
     }
 }
